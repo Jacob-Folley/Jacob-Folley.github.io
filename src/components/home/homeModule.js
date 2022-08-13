2e4ac221d89503893d7d47ac9550f1e187501092
Jacob-Folley <jakebfolley@gmail.com>
2022-08-12 21:53:39 -0500

    redesign of the entire website

--- src/components/home/homeModule.js
+++ src/components/home/homeModule.js
@@ -1,26 +1,213 @@
-import React, { useEffect, useState } from "react"
-import { NavBar } from '../navBar/navbar'
-import { ProjectModule } from '../projects/projectModule'
-import { SkillModule } from '../skills/skillModule'
-import { ContactModule } from '../contact/contact'
+import React, { useEffect, useState } from "react";
+import { NavBar } from "../navBar/navbar";
+import { ProjectModule } from "../projects/projectModule";
+import { SkillModule } from "../skills/skillModule";
+import { ContactModule } from "../contact/contact";
+import "react-slideshow-image/dist/styles.css";
+import { Slide } from "react-slideshow-image";
+import image from "../../img/Tsundoku-Home-Dark.png";
+import image1 from "../../img/Tsundoku-Home-Light.png";
+import image2 from "../../img/Tsundoku-List-Dark.png";
+import image3 from "../../img/Tsundoku-List-Light.png";
+import image4 from "../../img/Tsundoku-Page-Dark.png";
+import image5 from "../../img/Tsundoku-Page-Light.png";
+import image6 from "../../img/Tsundoku-Profile-Dark.png";
+import image7 from "../../img/Tsundoku-Profile-Light.png";
+
+import pic from "../../img/CareerConnectProfile.png";
+import pic1 from "../../img/CareerConnectSearch.png";
+import pic2 from "../../img/CareerConnectPost.png";
+import pic3 from "../../img/CareerConnectPostins.png";
+import pic4 from "../../img/CareerConnectApplicants.png";
 
 export const HomeModule = () => {
+  const slideImages = [
+    {
+      url: `${image}`,
+      caption: "Slide 1",
+    },
+    {
+      url: `${image1}`,
+      caption: "Slide 2",
+    },
+    {
+      url: `${image2}`,
+      caption: "Slide 3",
+    },
+    {
+      url: `${image3}`,
+      caption: "Slide 4",
+    },
+    {
+      url: `${image4}`,
+      caption: "Slide 5",
+    },
+    {
+      url: `${image5}`,
+      caption: "Slide 6",
+    },
+    {
+      url: `${image6}`,
+      caption: "Slide 7",
+    },
+    {
+      url: `${image7}`,
+      caption: "Slide 8",
+    },
+  ];
 
-    return (
-        <>
-            <NavBar />
-            <div className="homeContainer" id="homeAnchor">
-                <div className="introduction">
-                    <div className="top"><p>Hello, my name is</p></div>
-                    <div className="left"><h1>JACOB FOLLEY</h1></div>
-                    <div className="right"><h2>SOFTWARE ENGINEER</h2></div>
-                    <div className="bottom"><p id="bio">Based in Nashville</p></div>
+  const slideImages2 = [
+    {
+      url: `${pic}`,
+      caption: "Slide 1",
+    },
+    {
+      url: `${pic1}`,
+      caption: "Slide 2",
+    },
+    {
+      url: `${pic2}`,
+      caption: "Slide 3",
+    },
+    {
+      url: `${pic3}`,
+      caption: "Slide 4",
+    },
+    {
+      url: `${pic4}`,
+      caption: "Slide 5",
+    },
+  ];
+  return (
+    <>
+      <NavBar />
+      <div className="homeContainer">
+        <div className="introContainer" id="homeAnchor">
+          <div className="left">
+            <h1 className="intro">
+              Hello, my name is Jacob. I am a Software Engineer based in
+              Nashville. I enjoy creating software that helps make people's
+              lives better.{" "}
+            </h1>
+            <p>
+              If you wish to contact me, please email me at{" "}
+              <b>jakebfolley@gmail.com</b>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <section className="project">
+        <div className="projectInfo">
+          <h1 className="projectName">Tsundoku</h1>
+          <h3 className="projectSourceCode">
+            <b>Source Code: </b>
+            <a href="https://github.com/Jacob-Folley/CapStone-Client">GitHub</a>
+          </h3>
+
+          <div className="projectSkills">
+            <b>Technologies: </b>
+            React, JavaScript, HTML, CSS
+          </div>
+        </div>
+        <div className="projectPage">
+          <div className="slide-container">
+            <Slide>
+              {slideImages.map((slideImage, index) => (
+                <div className="each-slide" key={index}>
+                  <div
+                    className=".react-slideshow-container"
+                    style={{
+                      backgroundImage: `url(${slideImage.url})`,
+                      backgroundRepeat: "no-repeat",
+                      backgroundSize: "contain",
+                    }}
+                  >
+                    {/* <span>{slideImage.caption}</span> */}
+                  </div>
                 </div>
-            </div>
+              ))}
+            </Slide>
+          </div>
+        </div>
+      </section>
 
-            {/* <div id="projectAnchor"><ProjectModule /></div>
+      <section className="project">
+        <div className="projectInfo">
+          <h1 className="projectName">Career Connect</h1>
+          <h3 className="projectSourceCode">
+            <b>Source Code: </b>
+            <a href="https://github.com/Jacob-Folley/FrontEnd-Capstone">
+              GitHub
+            </a>
+          </h3>
+
+          <div className="projectSkills">
+            <b>Technologies: </b>
+            React, JavaScript, HTML, CSS, Python, Django
+          </div>
+        </div>
+        <div className="projectPage">
+          <div className="slide-container">
+            <Slide>
+              {slideImages2.map((slideImage, index) => (
+                <div className="each-slide" key={index}>
+                  <div
+                    className=".react-slideshow-container"
+                    style={{
+                      backgroundImage: `url(${slideImage.url})`,
+                      backgroundRepeat: "no-repeat",
+                      backgroundSize: "contain",
+                    }}
+                  >
+                    {/* <span>{slideImage.caption}</span> */}
+                  </div>
+                </div>
+              ))}
+            </Slide>
+          </div>
+        </div>
+      </section>
+
+      <div className="links">
+        <div className="social">
+          <a
+            href="https://docs.google.com/document/d/1vc-FrHDgSpF-oc-CAvBkGaflAUbF9qD30o6CKUx1UKs/edit?usp=sharing"
+            target="_blank"
+          >
+            <img
+              className="icon-resume"
+              src={require("../icons/icon_resume.png")}
+            />
+          </a>
+        </div>
+        <div className="social">
+          <a href="https://github.com/Jacob-Folley" target="_blank">
+            <img
+              className="icon-github"
+              src={require("../navBar/Octocat.png")}
+            />
+          </a>
+        </div>
+        <div className="social">
+          <a href="https://www.linkedin.com/in/jacobfolley/" target="_blank">
+            <img
+              className="icon-linkedin"
+              src={require("../LinkedIn-Logos/LI-In-Bug.png")}
+            />
+          </a>
+        </div>
+      </div>
+
+      {/* <div id="projectAnchor"><ProjectModule /></div>
             <div id="skillsAnchor"><SkillModule /></div>
             <div id="contactAnchor"><ContactModule /></div> */}
-        </>
-    )
-}
+
+      {/* <div className="imageContainer">
+            <div>
+              <img className="homeImage" src={image} />
+            </div>
+          </div> */}
+    </>
+  );
+};
--- src/components/navBar/navbar.js
+++ src/components/navBar/navbar.js
@@ -1,115 +1,125 @@
-import React, { useEffect, useState } from "react"
-import { useHistory, Link } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { useHistory, Link } from "react-router-dom";
 
 export const NavBar = () => {
-    const history = useHistory()
+  const history = useHistory();
 
-    const [state, setState] = useState('')
+  const [state, setState] = useState("");
 
-    const [change, setChange] = useState('')
+  const [change, setChange] = useState("");
 
-    const lightModeCheck = () => {
-        let lightMode = localStorage.getItem('lightMode')
-        return lightMode
+  const lightModeCheck = () => {
+    let lightMode = localStorage.getItem("lightMode");
+    return lightMode;
+  };
+
+  useEffect(() => {
+    setState(lightModeCheck());
+  }, []);
+
+  useEffect(() => {
+    if (change == "enabled") {
+    } else {
     }
+  }, []);
 
-    useEffect(
-        () => {
-            setState(lightModeCheck())
-        },
-        []
-    )
+  // lightModeCheck()
 
-    useEffect(
-        () => {
-            if (change == 'enabled') {
-                
-            } else {
-                
-            }
-        },
-        []
-    )
+  // check if dark mode is enabled
+  // if enabled, turn it off
+  // if enabled turn it onClick
 
-   
-    
+  const enableLightMode = () => {
+    document.body.classList.add("lightmode");
+    localStorage.setItem("lightMode", "enabled");
+  };
 
-    // lightModeCheck()
-    
+  const disableLightMode = () => {
+    document.body.classList.remove("lightmode");
+    localStorage.setItem("lightMode", null);
+  };
 
-    // check if dark mode is enabled
-    // if enabled, turn it off
-    // if enabled turn it onClick
+  if (lightModeCheck() === "enabled") {
+    enableLightMode();
+  }
 
-    const enableLightMode = () => {
-        document.body.classList.add('lightmode');
-        localStorage.setItem('lightMode', 'enabled')
+  const light = () => {
+    lightModeCheck();
+    console.log(lightModeCheck());
+    if (lightModeCheck() !== "enabled") {
+      enableLightMode();
     }
+  };
 
-    const disableLightMode = () => {
-        document.body.classList.remove('lightmode');
-        localStorage.setItem('lightMode', null)
+  const dark = () => {
+    lightModeCheck();
+    console.log(lightModeCheck());
+    if (lightModeCheck() == "enabled") {
+      disableLightMode();
     }
+  };
 
-    if (lightModeCheck() === "enabled") {
-        enableLightMode();
-    }
+  return (
+    <>
+      <section className="navBar">
+        <div className="navBarContainer">
+          <button
+            className="lightDark"
+            onClick={() => {
+              lightModeCheck();
+              console.log(lightModeCheck());
+              if (lightModeCheck() !== "enabled") {
+                enableLightMode();
+              } else {
+                disableLightMode();
+              }
+            }}
+          ></button>
+          <div className="iconHome">
+            <Link to="/">Jacob Folley</Link>
+          </div>
 
-    const light = () => {
-        lightModeCheck()
-        console.log(lightModeCheck())
-        if (lightModeCheck() !== 'enabled') {
-            enableLightMode();
-        }
-    }
+          <div className="iconCenter">
+            <div className="icon">
+              <Link to="/">work</Link>
+            </div>
+            <div className="icon">about me</div>
+          </div>
+        </div>
+      </section>
 
-    const dark = () => {
-        lightModeCheck()
-        console.log(lightModeCheck())
-        if (lightModeCheck() == 'enabled') {
-            disableLightMode();
-        }
-    }
-
-  
-   
-
-
-    return (
-        <>
-            <section className="navBar">
-
-                <button
-                    className="lightDark"
-                    onClick={() => {
-                        lightModeCheck()
-                        console.log(lightModeCheck())
-                        if (lightModeCheck() !== 'enabled') {
-                            enableLightMode();
-                        } else {
-                            disableLightMode();
-                        }
-                    }}
-                ></button>
-                <div className="iconHome">
-                <Link to="/">Jacob Folley</Link>
-                </div>
-
-                <div className="iconCenter">
-                    <div className="icon"><Link to="/projects">Projects</Link></div>
-                    <div className="icon"><Link to="/skills">Skills</Link></div>
-                    <div className="icon"><Link to="/contact">Contact</Link></div>
-                </div>
-
-
-
-                <div className="socials">
-                    <div className="social"><a href="https://docs.google.com/document/d/1vc-FrHDgSpF-oc-CAvBkGaflAUbF9qD30o6CKUx1UKs/edit?usp=sharing" target="_blank"><img className="icon-resume" src={require('../icons/icon_resume.png')} /></a></div>
-                    <div className="social"><a href="https://github.com/Jacob-Folley" target="_blank">{state == 'enabled' ? <img src={require('./GitHub-Mark-Light-32px.png')} /> : <img src={require('../icons/GitHub-Mark/PNG/GitHub-Mark-32px.png')} />}</a></div>
-                    <div className="social"><a href="https://www.linkedin.com/in/jacobfolley/" target="_blank"><img className="icon-linkedin" src={require('../LinkedIn-Logos/LI-In-Bug.png')} /></a></div>
-                </div>
-            </section>
-
-        </>
-    )
-}
+      {/* <div className="socials">
+            <div className="social">
+              <a
+                href="https://docs.google.com/document/d/1vc-FrHDgSpF-oc-CAvBkGaflAUbF9qD30o6CKUx1UKs/edit?usp=sharing"
+                target="_blank"
+              >
+                <img
+                  className="icon-resume"
+                  src={require("../icons/icon_resume.png")}
+                />
+              </a>
+            </div>
+            <div className="social">
+              <a href="https://github.com/Jacob-Folley" target="_blank">
+                <img
+                  className="icon-github"
+                  src={require("../navBar/Octocat.png")}
+                />
+              </a>
+            </div>
+            <div className="social">
+              <a
+                href="https://www.linkedin.com/in/jacobfolley/"
+                target="_blank"
+              >
+                <img
+                  className="icon-linkedin"
+                  src={require("../LinkedIn-Logos/LI-In-Bug.png")}
+                />
+              </a>
+            </div>
+          </div> */}
+    </>
+  );
+};
